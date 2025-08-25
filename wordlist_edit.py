c16365d404185eb85a04011294ee2ee0736227e7
Mark Schachner <markj.schachner@gmail.com>
2025-08-25 08:55:50 -0400

    wordlist edits

--- wordlist_edit.py
+++ wordlist_edit.py
@@ -1,26 +1,15 @@
-import re
+from blossom.wordlist import loadDict, updateWordlist
+from blossom.game import scoreWord
 
-input_path = "wordlist.txt"
-output_path = "wordlist.txt"
+def maxPossibleScore(word):
+    return max(scoreWord(word, letter, word) for letter in word)
 
-ansi_pattern = re.compile(r'(\x1b\[[0-9;]*m)(.*?)(\x1b\[[0-9;]*m)([^\w]*)$')
+def filterWeakWords():
+    dictionary = loadDict()
+    wordsToRemove = []
+    for word in dictionary:
+        if maxPossibleScore(word) < 15:
+            wordsToRemove.append(word)
+    updateWordlist([], wordsToRemove)
 
-def process_line(line):
-    match = ansi_pattern.match(line.rstrip('\n'))
-    if match:
-        _, text, _, trailing = match.groups()
-        return f"{text.lower()}{trailing}\n"
-    else:
-        # If no ANSI codes, just lowercase the word part
-        parts = re.match(r'(\w+)([^\w]*)$', line.rstrip('\n'))
-        if parts:
-            word, trailing = parts.groups()
-            return f"{word.lower()}{trailing}\n"
-        return line
-
-with open(input_path, "r") as infile:
-    lines = infile.readlines()
-
-with open(output_path, "w") as outfile:
-    for line in lines:
-        outfile.write(process_line(line))
+filterWeakWords()
